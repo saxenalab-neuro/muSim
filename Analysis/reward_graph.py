434004531a8000013560176c194b2d1e332ee322
Julian Theodore <jt2386@r817u09n01.misha.ycrc.yale.edu>
2025-07-23 11:35:10 -0400

    adding titles to graphs

--- Analysis/reward_graph.py
+++ Analysis/reward_graph.py
@@ -1,26 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.load('../training_statistics/Reach/stats_rewards.npy')
 
+def create_graph(load_path, save_path, env_name):
+    data = np.load(load_path)
 
-# Compute moving average
-window_size = 5000
-moving_avg = np.concatenate((np.zeros(window_size//2) * np.nan, np.convolve(data, np.ones(window_size)/window_size, mode='valid'), np.zeros(window_size//2) * np.nan))
+    # Compute moving average
+    window_size = 1000
+    moving_avg = np.concatenate((np.zeros(window_size//2) * np.nan, np.convolve(data, np.ones(window_size)/window_size, mode='valid'), np.zeros(window_size//2) * np.nan))
 
-# Trendline (linear, degree=1)
-x = np.arange(0, len(data))
-coeffs = np.polyfit(x, data, deg=1)
-trendline = np.polyval(coeffs, x)
+    # Trendline (linear, degree=1)
+    x = np.arange(0, len(data))
+    coeffs = np.polyfit(x, data, deg=1)
+    trendline = np.polyval(coeffs, x)
 
-plt.plot(data)
-plt.plot(moving_avg, label='Moving Average', color='red', linewidth=2)
-plt.plot(x, trendline, label='Trendline (Linear)', color='black', linestyle='--')
-plt.title('Reward Graph')
-plt.xlabel('Iteration')
-plt.ylabel('Reward')
-plt.grid(False)
-plt.tight_layout()
+    plt.plot(data)
+    plt.plot(moving_avg, label='Moving Average', color='red', linewidth=2)
+    plt.plot(x, trendline, label='Trendline (Linear)', color='black', linestyle='--')
+    plt.title(env_name + ' Reward Graph')
+    plt.xlabel('Iteration')
+    plt.ylabel('Reward')
+    plt.grid(False)
+    plt.tight_layout()
 
-plt.savefig('../training_statistics/reward_plot.png')  # Saves into train_statistics folder, not Analysis
-plt.close()
+    plt.savefig(save_path)  # Saves into train_statistics folder, not Analysis
+    plt.close()
+
+envs = ['Reach', 'CurvedReachClk', 'CurvedReachCClk', 'Sinusoid', 'SinusoidInv', \
+          'FullReach', 'CircleClk', 'CircleCClk', 'Figure8', 'Figure8Inv']
+
+for env in envs:
+    create_graph('../training_statistics/' + env + '/stats_rewards.npy', './graphs/' + env + '/reward_plot.png', env)
--- Analysis/visualize_kinematics.py
+++ Analysis/visualize_kinematics.py
@@ -12,111 +12,147 @@ import SAC.kinematics_preprocessing_specs
 
 import config
 
-from SAC.RL_Framework_Mujoco import DlyReach
+from SAC.RL_Framework_Mujoco import *
 
-parser = config.config_parser()
-args, unknown = parser.parse_known_args()
 
-#Select the target/marker for which to visualize the kinematics
-marker= 0
+def create_graphs(env, env_name, colors):
+    #Load the test data of usim
+    with open('../test_data/' + env_name + '/test_data.pkl', 'rb') as file:
+        test_data = pickle.load(file)
+        
+    kin_train = env.kin_to_sim
 
-#Load the test data of usim
-with open('../test_data/test_data.pkl', 'rb') as file:
-    test_data = pickle.load(file)
+    kin_agent = []
+
+    for idx, cond_kin in test_data['kinematics_mbodies'].items():
+        kin_agent.append(cond_kin[marker, :, :])
+
+    kin_sim = []
+
+    for idx, cond_kin in test_data['kinematics_mtargets'].items():
+        kin_sim.append(cond_kin[marker, :, :])
+
+    for cond in range(len(kin_agent)):
+        figure(figsize=(20, 5), dpi=80)
+
+        all_ranges = []
+        for coord in range(kin_agent[cond].shape[-1]):
+            values = np.concatenate([kin_sim[cond][:, coord], kin_agent[cond][:, coord]])
+            all_ranges.append(np.max(values) - np.min(values))
+        max_range = max(all_ranges)
+        
+        MSE_cond = 0
+        for coord in range(kin_agent[cond].shape[-1]):
+            
+            plt.subplot(1, kin_agent[cond].shape[-1], coord+1)
+            plt.plot(kin_sim[cond][:, coord], '--', label= 'Target')
+            plt.plot(kin_agent[cond][:, coord], label= 'Achieved')
+
+            sim_vals = kin_sim[cond][:, coord]
+            agent_vals = kin_agent[cond][:, coord]
+            
+            combined = np.concatenate([sim_vals, agent_vals])
+            mid = (np.max(combined) + np.min(combined)) / 2
+            plt.ylim(mid - max_range / 2, mid + max_range / 2)
+            
+            MSE_coord = MSE(kin_sim[cond][:, coord], kin_agent[cond][:, coord])
+            
+            plt.title(env_name + f' Kinematics for COND{cond+1} MARKER{marker} \n MSE{MSE_coord}')
+            
+            #omit the z coord for MSE calculations
+            if coord != 1:
+                MSE_cond += MSE_coord
+        
+        print(f'MSE for COND {cond}: ',  MSE_cond/2)
+        plt.legend()
+        plt.savefig('./graphs/' + env_name + f'/MSE for COND {cond}')
+
+
+    #Now plot the kinematics for all training conditions together
+
+    plt.figure(figsize= (10, 10))
+
+    for cond in range(len(kin_train)):
+        for coord in range((kin_agent[cond].shape[-1])):
+
+            if coord != 1:
+
+                kin_agent_coord = kin_agent[cond][:, coord]
+                kin_sim_coord = kin_sim[cond][:, coord]
+
+                # Normalize for pretty display
+                norm_factor = np.min(kin_sim_coord) + np.ptp(kin_sim_coord) / 2
+
+                kin_agent_coord = (kin_agent_coord - norm_factor)
+                kin_sim_coord = (kin_sim_coord - norm_factor)
+
+                if coord == 0:
+                    col = (0 / 255, 191 / 255, 255 / 255)
+                else:
+                    col = (205 / 255, 133 / 255, 63 / 255)
+
+                plt.plot(kin_agent_coord - cond*0.1, '-', linewidth= 1.5, c=col)
+                plt.plot(kin_sim_coord - cond*0.1, '--', linewidth= 1.5, c=col)
+
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.title(env_name + ' Kinematics Multiple Graphs')
+    plt.legend(['Achieved', 'Target'])
+    plt.savefig('./graphs/' + env_name + '/Kinematics Multiple Graphs')
+
+    #Now plot the kinematics for all training conditions together
+
+    plt.figure(figsize= (10, 10))
+
+    for cond in range(len(kin_train)):
+        kin_agent_coord = kin_agent[cond]
+        plt.plot(kin_agent_coord[:, 0], kin_agent_coord[:, 2], '-', linewidth=3, c=colors[cond][0])
+        kin_sim_coord = kin_sim[cond]
+        plt.plot(kin_sim_coord[:, 0], kin_sim_coord[:, 2], '--', linewidth=3, c=colors[cond][1])
+
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.title(env_name + ' Kinematics Single Graph')
+    plt.savefig('./graphs/' + env_name + '/Kinematics Single Graph')
+
+    plt.close()
+
+
+if __name__ == '__main__':
+    parser = config.config_parser()
+    args, unknown = parser.parse_known_args()
+
+    #Select the target/marker for which to visualize the kinematics
+    marker = 0
+
+    env_names = ['Reach', 'CurvedReachClk', 'CurvedReachCClk', 'Sinusoid', 'SinusoidInv', \
+          'FullReach', 'Figure8', 'Figure8Inv']
+
+    envs = [DlyReach, DlyCurvedReachClk, DlyCurvedReachCClk, DlySinusoid, DlySinusoidInv, \
+                DlyFullReach, DlyFigure8, DlyFigure8Inv]
+
+    env_names = ['CircleClk', 'CircleCClk']
+
+    envs = [DlyCircleClk, DlyCircleCClk]
+
+    color_pairs = [
+    ((0.121, 0.466, 0.705), (0.4, 0.65, 0.85)),   # blues
+    ((1.0, 0.498, 0.054), (1.0, 0.65, 0.3)),      # oranges
+    ((0.172, 0.627, 0.172), (0.4, 0.8, 0.4)),     # greens
+    ((0.839, 0.152, 0.156), (0.9, 0.4, 0.4)),     # reds
+    ((0.580, 0.404, 0.741), (0.7, 0.6, 0.9)),     # purples
+    ((0.549, 0.337, 0.294), (0.7, 0.5, 0.4)),     # browns
+    ((0.890, 0.467, 0.761), (0.95, 0.65, 0.85)),  # pinks
+    ((0.498, 0.498, 0.498), (0.7, 0.7, 0.7)),     # grays
+    ]
+
+    assert len(env_names) == len(envs)
+
+    args.initial_pose_path = '../' + args.initial_pose_path
     
-args.initial_pose_path = '../' + args.initial_pose_path
-env = DlyReach('../' + args.musculoskeletal_model_path[:-len('musculoskeletal_model.xml')] + 'musculo_targets.xml', 1, args)
-kin_train = env.kin_to_sim
-
-kin_agent = []
-
-for idx, cond_kin in test_data['kinematics_mbodies'].items():
-    kin_agent.append(cond_kin[marker, :, :])
-
-kin_sim = []
-
-for idx, cond_kin in test_data['kinematics_mtargets'].items():
-    kin_sim.append(cond_kin[marker, :, :])
-
-for cond in range(len(kin_agent)):
-    figure(figsize=(20, 5), dpi=80)
-
-    all_ranges = []
-    for coord in range(kin_agent[cond].shape[-1]):
-        values = np.concatenate([kin_sim[cond][:, coord], kin_agent[cond][:, coord]])
-        all_ranges.append(np.max(values) - np.min(values))
-    max_range = max(all_ranges)
-    
-    MSE_cond = 0
-    for coord in range(kin_agent[cond].shape[-1]):
+    for i in range(len(env_names)):
         
-        plt.subplot(1, kin_agent[cond].shape[-1], coord+1)
-        plt.plot(kin_sim[cond][:, coord], '--', label= 'Target')
-        plt.plot(kin_agent[cond][:, coord], label= 'Achieved')
-
-        sim_vals = kin_sim[cond][:, coord]
-        agent_vals = kin_agent[cond][:, coord]
-        
-        combined = np.concatenate([sim_vals, agent_vals])
-        mid = (np.max(combined) + np.min(combined)) / 2
-        plt.ylim(mid - max_range / 2, mid + max_range / 2)
-        
-        MSE_coord = MSE(kin_sim[cond][:, coord], kin_agent[cond][:, coord])
-        
-        plt.title(f'Kinematics for COND{cond+1} MARKER{marker} \n MSE{MSE_coord}')
-        
-        #omit the z coord for MSE calculations
-        if coord != 1:
-            MSE_cond += MSE_coord
-    
-    print(f'MSE for COND {cond}: ',  MSE_cond/2)
-    plt.legend()
-    plt.savefig(f'MSE for COND {cond}')
-
-
-#Now plot the kinematics for all training conditions together
-
-plt.figure(figsize= (10, 10))
-
-for cond in range(len(kin_train)):
-    for coord in range((kin_agent[cond].shape[-1])):
-
-        if coord != 1:
-
-            kin_agent_coord = kin_agent[cond][:, coord]
-            kin_sim_coord = kin_sim[cond][:, coord]
-
-            # Normalize for pretty display
-            norm_factor = np.min(kin_sim_coord) + np.ptp(kin_sim_coord) / 2
-
-            kin_agent_coord = (kin_agent_coord - norm_factor)
-            kin_sim_coord = (kin_sim_coord - norm_factor)
-
-            if coord == 0:
-                col = (0 / 255, 191 / 255, 255 / 255)
-            else:
-                col = (205 / 255, 133 / 255, 63 / 255)
-
-            plt.plot(kin_agent_coord - cond*0.1, '-', linewidth= 1.5, c=col)
-            plt.plot(kin_sim_coord - cond*0.1, '--', linewidth= 1.5, c=col)
-
-plt.xticks([])
-plt.yticks([])
-
-plt.title('Kinematics Multiple Graphs')
-plt.legend(['Achieved', 'Target'])
-plt.savefig('Kinematics Multiple Graphs')
-
-#Now plot the kinematics for all training conditions together
-
-plt.figure(figsize= (10, 10))
-
-for cond in range(len(kin_train)):
-    kin_agent_coord = kin_agent[cond]
-    plt.plot(kin_agent_coord[:, 0], kin_agent_coord[:, 1], linewidth=4)
-
-plt.xticks([])
-plt.yticks([])
-
-plt.title('Kinematics Single Graph')
-plt.savefig('Kinematics Single Graph')
+        env = envs[i]('../' + args.musculoskeletal_model_path[:-len('musculoskeletal_model.xml')] + 'musculo_targets.xml', 1, args)
+        create_graphs(env, env_names[i], color_pairs)
